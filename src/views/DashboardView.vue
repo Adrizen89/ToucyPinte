6658a0ce2926beebc94083f7b2db3d3e1a29c17a
Adrizen89 <adrienberard@hotmail.fr>
2024-10-23 15:30:07 +0200

    Add back for dashboard

--- src/views/DashboardView.vue
+++ src/views/DashboardView.vue
@@ -24,48 +24,143 @@
       <h1 class="text-2xl font-bold text-sky-50 text-center">ToucyPinte</h1>
     </header>
 
-    <!-- Table: Liste des Membres -->
+    <!-- Table: Liste des Transactions -->
     <div class="mb-8">
-      <h2 class="text-lg font-semibold text-sky-50 mb-4">Liste des Membres</h2>
-      <!-- Vérification si les membres existent avant d'afficher la table -->
+      <h2 class="text-lg font-semibold text-sky-50 mb-4">
+        Liste des Transactions
+      </h2>
+
       <table
-        v-if="members && members.length > 0"
+        v-if="paginatedTransactions && paginatedTransactions.length > 0"
         class="min-w-full bg-sky-50 border rounded-lg shadow-md"
       >
         <thead>
           <tr>
             <th
               class="px-4 py-2 text-left text-sky-950 border-b cursor-pointer"
-              @click="sortBy('name', 'members')"
+              @click="sortBy('name', 'transactions')"
             >
-              Nom
-              <span v-if="sortKeyMembers === 'name'">{{
-                sortDirectionMembers === 'asc' ? '▲' : '▼'
+              Toucycois
+              <span v-if="sortKeyTransactions === 'name'">{{
+                sortDirectionTransactions === 'asc' ? '▲' : '▼'
               }}</span>
             </th>
             <th
               class="px-4 py-2 text-left text-sky-950 border-b cursor-pointer"
-              @click="sortBy('totalPaid', 'members')"
+              @click="sortBy('montant', 'transactions')"
             >
-              Total Payé
-              <span v-if="sortKeyMembers === 'totalPaid'">{{
-                sortDirectionMembers === 'asc' ? '▲' : '▼'
+              Montant
+              <span v-if="sortKeyTransactions === 'montant'">{{
+                sortDirectionTransactions === 'asc' ? '▲' : '▼'
               }}</span>
             </th>
             <th
               class="px-4 py-2 text-left text-sky-950 border-b cursor-pointer"
-              @click="sortBy('totalDebt', 'members')"
+              @click="sortBy('date', 'transactions')"
             >
-              Total Dette
-              <span v-if="sortKeyMembers === 'totalDebt'">{{
-                sortDirectionMembers === 'asc' ? '▲' : '▼'
+              Date
+              <span v-if="sortKeyTransactions === 'date'">{{
+                sortDirectionTransactions === 'asc' ? '▲' : '▼'
               }}</span>
             </th>
           </tr>
         </thead>
         <tbody>
           <tr
-            v-for="(member, index) in paginatedMembers"
+            v-for="(tournee, index) in paginatedTransactions"
+            :key="index"
+            class="hover:bg-gray-100"
+          >
+            <td class="px-4 py-2 border-b text-sky-900">{{ tournee.name }}</td>
+            <td class="px-4 py-2 border-b text-sky-800">
+              {{ tournee.montant }}€
+            </td>
+            <td class="px-4 py-2 border-b text-sky-800">{{ tournee.date }}</td>
+          </tr>
+          <tr
+            v-for="n in emptyRows('paginatedTransactions')"
+            :key="'empty-transactions-' + n"
+          >
+            <td class="px-4 py-2 border-b text-sky-900">&nbsp;</td>
+            <td class="px-4 py-2 border-b text-sky-800">&nbsp;</td>
+            <td class="px-4 py-2 border-b text-sky-800">&nbsp;</td>
+          </tr>
+        </tbody>
+      </table>
+
+      <p v-else class="text-white">
+        Aucune transaction trouvée pour l'instant.
+      </p>
+
+      <div
+        class="flex justify-between items-center mt-4"
+        v-if="totalPagesTransactions > 1"
+      >
+        <button
+          class="btn-pagination"
+          :disabled="currentPageTransactions === 1"
+          @click="prevPage('transactions')"
+        >
+          Précédent
+        </button>
+        <span class="text-gray-600"
+          >Page {{ currentPageTransactions }} sur
+          {{ totalPagesTransactions }}</span
+        >
+        <button
+          class="btn-pagination"
+          :disabled="currentPageTransactions === totalPagesTransactions"
+          @click="nextPage('transactions')"
+        >
+          Suivant
+        </button>
+      </div>
+    </div>
+
+    <!-- Table: Liste des Membres avec leurs totaux -->
+    <div class="mb-8">
+      <h2 class="text-lg font-semibold text-sky-50 mb-4">
+        Liste des Toucycois
+      </h2>
+
+      <table
+        v-if="paginatedMembres && paginatedMembres.length > 0"
+        class="min-w-full bg-sky-50 border rounded-lg shadow-md"
+      >
+        <thead>
+          <tr>
+            <th
+              class="px-4 py-2 text-left text-sky-950 border-b cursor-pointer"
+              @click="sortBy('name', 'membres')"
+            >
+              Toucycois
+              <span v-if="sortKeyMembres === 'name'">{{
+                sortDirectionMembres === 'asc' ? '▲' : '▼'
+              }}</span>
+            </th>
+            <th
+              class="px-4 py-2 text-left text-sky-950 border-b cursor-pointer"
+              @click="sortBy('totalPaid', 'membres')"
+            >
+              Total payé
+              <span v-if="sortKeyMembres === 'totalPaid'">{{
+                sortDirectionMembres === 'asc' ? '▲' : '▼'
+              }}</span>
+            </th>
+            <th
+              class="px-4 py-2 text-left text-sky-950 border-b cursor-pointer"
+              @click="sortBy('totalDebt', 'membres')"
+            >
+              Total Dette
+              <span v-if="sortKeyMembres === 'totalDebt'">{{
+                sortDirectionMembres === 'asc' ? '▲' : '▼'
+              }}</span>
+            </th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr
+            v-for="(member, index) in paginatedMembres"
             :key="index"
             class="hover:bg-gray-100"
           >
@@ -77,34 +172,132 @@
               {{ member.totalDebt }}€
             </td>
           </tr>
+          <tr
+            v-for="n in emptyRows('paginatedMembres')"
+            :key="'empty-membres-' + n"
+          >
+            <td class="px-4 py-2 border-b text-sky-900">&nbsp;</td>
+            <td class="px-4 py-2 border-b text-sky-800">&nbsp;</td>
+            <td class="px-4 py-2 border-b text-sky-800">&nbsp;</td>
+          </tr>
         </tbody>
       </table>
-      <!-- Message à afficher si aucun membre n'est trouvé -->
-      <p v-else class="text-white">Aucun membre trouvé pour l'instant.</p>
+
+      <p v-else class="text-white">
+        Aucune transaction trouvée pour l'instant.
+      </p>
+
+      <div
+        class="flex justify-between items-center mt-4"
+        v-if="totalPagesMembres > 1"
+      >
+        <button
+          class="btn-pagination"
+          :disabled="currentPageMembres === 1"
+          @click="prevPage('membres')"
+        >
+          Précédent
+        </button>
+        <span class="text-gray-600"
+          >Page {{ currentPageMembres }} sur {{ totalPagesMembres }}</span
+        >
+        <button
+          class="btn-pagination"
+          :disabled="currentPageMembres === totalPagesMembres"
+          @click="nextPage('membres')"
+        >
+          Suivant
+        </button>
+      </div>
     </div>
 
-    <!-- Pagination pour les membres -->
-    <div
-      v-if="members && members.length > rowLimit"
-      class="flex justify-between items-center mt-4"
-    >
-      <button
-        @click="prevPage('members')"
-        :disabled="currentPageMembers === 1"
-        class="btn-pagination"
+    <!-- Table: Liste des Dettes -->
+    <div class="mb-8">
+      <h2 class="text-lg font-semibold text-sky-50 mb-4">Liste des Dettes</h2>
+
+      <table
+        v-if="paginatedDebts && paginatedDebts.length > 0"
+        class="min-w-full bg-sky-50 border rounded-lg shadow-md"
       >
-        Précédent
-      </button>
-      <span class="text-gray-600"
-        >Page {{ currentPageMembers }} sur {{ totalPagesMembers }}</span
+        <thead>
+          <tr>
+            <th
+              class="px-4 py-2 text-left text-sky-950 border-b cursor-pointer"
+              @click="sortBy('name', 'debts')"
+            >
+              Toucycois
+              <span v-if="sortKeyDebts === 'name'">{{
+                sortDirectionDebts === 'asc' ? '▲' : '▼'
+              }}</span>
+            </th>
+            <th
+              class="px-4 py-2 text-left text-sky-950 border-b cursor-pointer"
+              @click="sortBy('montantDette', 'debts')"
+            >
+              Montant Dette
+              <span v-if="sortKeyDebts === 'montantDette'">{{
+                sortDirectionDebts === 'asc' ? '▲' : '▼'
+              }}</span>
+            </th>
+            <th
+              class="px-4 py-2 text-left text-sky-950 border-b cursor-pointer"
+              @click="sortBy('date', 'debts')"
+            >
+              Date
+              <span v-if="sortKeyDebts === 'date'">{{
+                sortDirectionDebts === 'asc' ? '▲' : '▼'
+              }}</span>
+            </th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr
+            v-for="(debt, index) in paginatedDebts"
+            :key="index"
+            class="hover:bg-gray-100"
+          >
+            <td class="px-4 py-2 border-b text-sky-900">{{ debt.name }}</td>
+            <td class="px-4 py-2 border-b text-sky-800">
+              {{ debt.montantDette }}€
+            </td>
+            <td class="px-4 py-2 border-b text-sky-800">{{ debt.date }}</td>
+          </tr>
+          <!-- Ajouter des lignes vides si nécessaire -->
+          <tr
+            v-for="n in emptyRows('paginatedDebts')"
+            :key="'empty-dettes-' + n"
+          >
+            <td class="px-4 py-2 border-b text-sky-900">&nbsp;</td>
+            <td class="px-4 py-2 border-b text-sky-800">&nbsp;</td>
+            <td class="px-4 py-2 border-b text-sky-800">&nbsp;</td>
+          </tr>
+        </tbody>
+      </table>
+
+      <p v-else class="text-white">Aucune dette trouvée pour l'instant.</p>
+
+      <div
+        class="flex justify-between items-center mt-4"
+        v-if="totalPagesDebts > 1"
       >
-      <button
-        @click="nextPage('members')"
-        :disabled="currentPageMembers === totalPagesMembers"
-        class="btn-pagination"
-      >
-        Suivant
-      </button>
+        <button
+          class="btn-pagination"
+          :disabled="currentPageDebts === 1"
+          @click="prevPage('debts')"
+        >
+          Précédent
+        </button>
+        <span class="text-gray-600"
+          >Page {{ currentPageDebts }} sur {{ totalPagesDebts }}</span
+        >
+        <button
+          class="btn-pagination"
+          :disabled="currentPageDebts === totalPagesDebts"
+          @click="nextPage('debts')"
+        >
+          Suivant
+        </button>
+      </div>
     </div>
   </div>
 </template>
@@ -116,54 +309,186 @@ import { collection, getDocs } from 'firebase/firestore'
 export default {
   data() {
     return {
-      members: [], // Initialisation de la liste des membres comme tableau vide
-      sortKeyMembers: '',
-      sortDirectionMembers: 'asc',
+      transactions: [],
+      sortKeyTransactions: '',
+      sortDirectionTransactions: 'asc',
+      sortKeyMembres: '',
+      sortDirectionMembres: 'asc',
+      sortKeyDebts: '',
+      sortDirectionDebts: 'asc',
+      currentPageTransactions: 1,
+      currentPageDebts: 1,
+      currentPageMembres: 1,
+      itemsPerPage: 5,
     }
   },
   computed: {
-    paginatedMembers() {
-      const sortedData = this.sortData(this.members, 'members')
-      return sortedData
+    emptyRows() {
+      return table => {
+        const paginatedData = this[table]
+        return this.itemsPerPage - paginatedData.length
+      }
+    },
+    filteredMembers() {
+      const membersTotals = {}
+
+      // Calculer le total payé et total dette pour chaque membre
+      for (const tournee of this.transactions) {
+        if (!membersTotals[tournee.name]) {
+          membersTotals[tournee.name] = {
+            name: tournee.name,
+            totalPaid: 0,
+            totalDebt: 0,
+          }
+        }
+
+        // Si c'est payé, on ajoute au total payé
+        if (tournee.paye) {
+          membersTotals[tournee.name].totalPaid += parseFloat(tournee.montant)
+        }
+
+        // Si c'est une dette, on ajoute au total des dettes
+        if (tournee.dette) {
+          membersTotals[tournee.name].totalDebt += parseFloat(
+            tournee.montantDette,
+          )
+        }
+      }
+
+      // Retourner les membres avec leur total payé et dette
+      return Object.values(membersTotals)
+    },
+    paginatedMembres() {
+      const sortedMembres = this.sortedMembres
+      const start = (this.currentPageMembres - 1) * this.itemsPerPage
+      const end = start + this.itemsPerPage
+      return sortedMembres.slice(start, end)
+    },
+    paginatedTransactions() {
+      const sortedTransactions = this.sortedTransactions
+      const start = (this.currentPageTransactions - 1) * this.itemsPerPage
+      const end = start + this.itemsPerPage
+      return sortedTransactions.slice(start, end)
+    },
+    paginatedDebts() {
+      const sortedDebts = this.sortedDebts
+      const start = (this.currentPageDebts - 1) * this.itemsPerPage
+      const end = start + this.itemsPerPage
+      return sortedDebts.slice(start, end)
+    },
+    totalPagesTransactions() {
+      return Math.ceil(this.transactions.length / this.itemsPerPage)
+    },
+    totalPagesMembres() {
+      return Math.ceil(this.filteredMembers.length / this.itemsPerPage)
+    },
+    totalPagesDebts() {
+      return Math.ceil(this.filteredDebts.length / this.itemsPerPage)
+    },
+    filteredDebts() {
+      return this.transactions.filter(tournee => tournee.montantDette > 0)
+    },
+    sortedTransactions() {
+      return [...this.transactions].sort((a, b) => {
+        let modifier = this.sortDirectionTransactions === 'asc' ? 1 : -1
+        if (a[this.sortKeyTransactions] < b[this.sortKeyTransactions])
+          return -1 * modifier
+        if (a[this.sortKeyTransactions] > b[this.sortKeyTransactions])
+          return 1 * modifier
+        return 0
+      })
+    },
+    sortedMembres() {
+      return [...this.filteredMembers].sort((a, b) => {
+        let modifier = this.sortDirectionMembres === 'asc' ? 1 : -1
+        if (a[this.sortKeyMembres] < b[this.sortKeyMembres])
+          return -1 * modifier
+        if (a[this.sortKeyMembres] > b[this.sortKeyMembres]) return 1 * modifier
+        return 0
+      })
+    },
+    sortedDebts() {
+      return [...this.filteredDebts].sort((a, b) => {
+        let modifier = this.sortDirectionDebts === 'asc' ? 1 : -1
+        if (a[this.sortKeyDebts] < b[this.sortKeyDebts]) return -1 * modifier
+        if (a[this.sortKeyDebts] > b[this.sortKeyDebts]) return 1 * modifier
+        return 0
+      })
     },
   },
   methods: {
-    async fetchMembers() {
+    async fetchTransac() {
       try {
-        const membersCollection = collection(db, 'members') // Assure-toi que le nom de la collection est correct
-        const membersSnapshot = await getDocs(membersCollection)
-        this.members = membersSnapshot.docs.map(doc => doc.data()) // Convertit les données récupérées
+        const tourneesCollection = collection(db, 'tournees')
+        const tourneesSnapshot = await getDocs(tourneesCollection)
+        this.transactions = tourneesSnapshot.docs.map(doc => ({
+          ...doc.data(),
+          id: doc.id,
+        }))
       } catch (error) {
-        console.error('Erreur lors de la récupération des membres:', error)
+        console.error('Erreur lors de la récupération des transactions:', error)
       }
     },
-    sortBy(key, table) {
-      if (table === 'members') {
-        if (this.sortKeyMembers === key) {
-          this.sortDirectionMembers =
-            this.sortDirectionMembers === 'asc' ? 'desc' : 'asc'
+    sortBy(key, type) {
+      if (type === 'transactions') {
+        if (this.sortKeyTransactions === key) {
+          this.sortDirectionTransactions =
+            this.sortDirectionTransactions === 'asc' ? 'desc' : 'asc'
         } else {
-          this.sortKeyMembers = key
-          this.sortDirectionMembers = 'asc'
+          this.sortKeyTransactions = key
+          this.sortDirectionTransactions = 'asc'
+        }
+      } else if (type === 'membres') {
+        if (this.sortKeyMembres === key) {
+          this.sortDirectionMembres =
+            this.sortDirectionMembres === 'asc' ? 'desc' : 'asc'
+        } else {
+          this.sortKeyMembres = key
+          this.sortDirectionMembres = 'asc'
+        }
+      } else if (type === 'debts') {
+        if (this.sortKeyDebts === key) {
+          this.sortDirectionDebts =
+            this.sortDirectionDebts === 'asc' ? 'desc' : 'asc'
+        } else {
+          this.sortKeyDebts = key
+          this.sortDirectionDebts = 'asc'
         }
       }
     },
-    sortData(data) {
-      if (!this.sortKeyMembers) return data
-      return data.slice().sort((a, b) => {
-        if (this.sortDirectionMembers === 'asc') {
-          return a[this.sortKeyMembers] > b[this.sortKeyMembers] ? 1 : -1
-        } else {
-          return a[this.sortKeyMembers] < b[this.sortKeyMembers] ? 1 : -1
-        }
-      })
+    nextPage(table) {
+      if (
+        table === 'transactions' &&
+        this.currentPageTransactions < this.totalPagesTransactions
+      ) {
+        this.currentPageTransactions++
+      } else if (
+        table === 'membres' &&
+        this.currentPageMembres < this.totalPagesMembres
+      ) {
+        this.currentPageMembres++
+      } else if (
+        table === 'debts' &&
+        this.currentPageDebts < this.totalPagesDebts
+      ) {
+        this.currentPageDebts++
+      }
+    },
+    prevPage(table) {
+      if (table === 'transactions' && this.currentPageTransactions > 1) {
+        this.currentPageTransactions--
+      } else if (table === 'membres' && this.currentPageMembres > 1) {
+        this.currentPageMembres--
+      } else if (table === 'debts' && this.currentPageDebts > 1) {
+        this.currentPageDebts--
+      }
     },
     goHome() {
       this.$router.push('/')
     },
   },
   mounted() {
-    this.fetchMembers() // Récupérer les membres dès que le composant est monté
+    this.fetchTransac()
   },
 }
 </script>
@@ -179,10 +504,10 @@ export default {
   transition: background-color 0.2s;
 }
 .btn-pagination:hover {
-  background-color: #2d3748;
+  background-color: #3b82f6;
 }
 .btn-pagination:disabled {
-  background-color: #0369a1;
+  background-color: #075985;
   cursor: not-allowed;
 }
 </style>
